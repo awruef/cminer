bb43fe24d185197131eb441c1ed861fd77e9bef2
Andrew Ruef <2530731+awruef@users.noreply.github.com>
2019-01-08 16:33:22 -0500

    .

--- query.py
+++ query.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python2.7 
+from StringIO import StringIO
 import subprocess
 import tempfile
 import argparse
 import json
+import csv
 import sys
 import os
 
 CLANG_QUERY_CMD = "clang-query -p={db} -f={q} {f}"
 
 def parse_results(r):
+    rf = StringIO(r)
     rt = []
-    idx = r.find("Match #")
-    while idx != -1:
-        f1 = r.find("\n", idx)
-        f2 = r.find("Match #", f1)
-        rt.append(r[f1+1:f2].strip().rstrip())
-        idx = f2
+    rd = csv.reader(rf)
+    for l in rd:
+        rt.append((l[0],l[1]))
     return rt
 
 def run_query(query, database, file):
     # Write query out into a named temporary file. 
     nt = tempfile.NamedTemporaryFile(delete=False)
+    # This assumes we have a patched clang-query that can output in csv.
+    nt.write("set output csv\n")
     nt.write(query)
+    nt.write("\n")
     ntnm = nt.name
     nt.close()
 
@@ -29,7 +32,7 @@ def run_query(query, database, file):
     c = CLANG_QUERY_CMD.format(db=database, f=file, q=ntnm)
     print c
     r = subprocess.check_output(c, shell=True)
-    os.unlink(ntnm)
+    #os.unlink(ntnm)
     return parse_results(r)
 
 def main(args):
